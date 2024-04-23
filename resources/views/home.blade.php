9e2af4b5502eea44fa7ba6b2e63ea0c276cd5523
ahmadhafidz04 <ahmad.hafidz04@outlook.com>
2024-04-23 07:24:27 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>My Profile | Ahmad Hafidz</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+  </head>
+  <body>
+    
+    <!--Navbar Awal-->
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">pid.</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#jumbotron">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#projects">Project</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#experience">Experience</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#education">Education</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!--Navbar Akhir-->
+    <!-- HEADER SECTION START -->
+    <section id="jumbotron" class="jumbotron text-center">
+      <img src="Hafidz.png" class="rounded-circle m-3" alt="Ahmad Hafidz" width="200" height="200">
+      <h1 class="display-4 fw-bold">Ahmad Hafidz</h1>
+      <p class="lead">Student | Web Developer</p>
+      <div class="social-icons">
+        <a href="https://www.instagram.com/hfdzgdm_/" class="social-icon"><i class="fab fa-instagram m-3"></i></a>
+        <a href="https://www.linkedin.com/in/ahfidz/" class="social-icon"><i class="fab fa-linkedin-in m-3"></i></a>
+        <a href="https://github.com/ahmadhafidz04" class="social-icon"><i class="fab fa-github m-3"></i></a>
+      </div>
+    </section>
+    <!-- HEADER SECTION END -->
+
+    <!-- ABOUT SECTION START -->
+    <section id="about" class="mt-5 pt-5 mb-5 pb-5">
+      <div class="container">
+        <!-- <div class="row text-center">
+          <div class="col">
+            <h2>Information</h2>
+          </div>
+        </div> -->
+        <div class="row justify-content-evenly">
+          <div class="col-md-4">
+            <h2>Information</h2>
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">Name: Ahmad Hafidz</p>
+                <p class="card-text">Age: 20</p>
+                <p class="card-text">Email : iniemail@example.com</p>
+                <p class="card-text">Phone : XXXXXXXXXXXX</p>
+                <p class="card-text">Language : English, Indonesia, Javanese</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-8">
+            <h2>About</h2>
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">Hello! I'm Ahmad Hafidz, also known as Hafidz or Hapid. As a dedicated web developer and current Computer Science student, I'm passionate about crafting immersive online experiences and delving into the depths of technology to innovate and create. With a blend of practical skills and theoretical knowledge, I strive to push boundaries and harness the power of code to build solutions that make a meaningful impact. Let's embark on this journey together, transforming ideas into reality one line of code at a time.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- ABOUT SECTION END -->
+    <!-- EDUCATION SECTION START -->
+    <section id="edu-exp">
+      <div class="container">
+
+        <section id="education">
+          <div class="row">
+            <div class="col">
+              <h2 class="mb-5">Education</h2>
+              
+              <div class="content">
+                <div class="box md-6">
+                  <h4>2010 - 2016</h4>
+                  <h3>SD Muhammadiyah 08 Dau Malang</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil repellat laudantium, eaque maxime inventore delectus deleniti aliquid laboriosam dolorem consequatur.</p>
+                </div>
+
+                <div class="box md-6">
+                  <h4>2016 - 2019</h4>
+                  <h3>MTs Surya Buana Malang</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil repellat laudantium, eaque maxime inventore delectus deleniti aliquid laboriosam dolorem consequatur.</p>
+                </div>
+
+                <div class="box md-6">
+                  <h4>2019 - 2022</h4>
+                  <h3>MAN 1 Kota Malang</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil repellat laudantium, eaque maxime inventore delectus deleniti aliquid laboriosam dolorem consequatur.</p>
+                </div>
+                
+                <div class="box md-6">
+                  <h4>2022 - Now</h4>
+                  <h3>Bina Nusantara University</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil repellat laudantium, eaque maxime inventore delectus deleniti aliquid laboriosam dolorem consequatur.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- EDUCATION SECTION END -->
+        <!-- EXPERIENCE SECTION START -->
+        <section id="experience">
+          <div class="row">
+            <div class="col">
+              <h2 class="mb-5">Experience</h2>
+              
+              <div class="content">
+                <div class="box md-6">
+                  <h4>2010 - 2016</h4>
+                  <h3>UI/UX Designer</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil repellat laudantium, eaque maxime inventore delectus deleniti aliquid laboriosam dolorem consequatur.</p>
+                </div>
+
+                <div class="box md-6">
+                  <h4>2016 - 2019</h4>
+                  <h3>Front-end Developer</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil repellat laudantium, eaque maxime inventore delectus deleniti aliquid laboriosam dolorem consequatur.</p>
+                </div>
+                
+                <div class="box md-6">
+                  <h4>2019 - 2022</h4>
+                  <h3>Full-stack Developer</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil repellat laudantium, eaque maxime inventore delectus deleniti aliquid laboriosam dolorem consequatur.</p>
+                </div>
+
+                <div class="box md-6">
+                  <h4>2022 - Now</h4>
+                  <h3>Project Manager</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil repellat laudantium, eaque maxime inventore delectus deleniti aliquid laboriosam dolorem consequatur.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </section>
+    <!-- EXPERIENCE SECTION END -->
+    <!-- PROJECT SECTION START -->
+    <section id="projects" class="m-5">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col mt-5">
+            <h2>Projects</h2>
+          </div>
+        </div>
+    
+        <div class="row p-5 justify-content-center">
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="NotFoundBg.jpeg" class="card-img-top" alt="NotFoundBg.jpeg">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="NotFoundBg.jpeg" class="card-img-top" alt="NotFoundBg.jpeg">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="NotFoundBg.jpeg" class="card-img-top" alt="NotFoundBg.jpeg">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="NotFoundBg.jpeg" class="card-img-top" alt="NotFoundBg.jpeg">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="NotFoundBg.jpeg" class="card-img-top" alt="NotFoundBg.jpeg">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <!-- Repeat similar structure for additional cards -->
+        </div>
+      </div>
+    </section>
+    <!-- PROJECT SECTION END -->
+    <!-- CONTACT SECTION START -->
+
+    <footer>
+      <div class="container">
+        <p class="text-center">Copyright &copy; 2023 Ahmad Hafidz. All Rights Reserved.</p>
+      </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
